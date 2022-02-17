3754e1d12ff2b641deaa354f7f8f982718f3258a
Vinícius Carneiro <44946693+carneirodev@users.noreply.github.com>
2022-02-17 03:58:22 -0300

    feat: schedules

--- index.js
+++ index.js
@@ -54,7 +54,9 @@ function updateTask(){
 function saveTask(e) {
   let data = document.getElementById('data').value;
   let hour = document.getElementById('horario').value;
-  let owner_id = "5f932006-0024-45ff-b351-04119a2d6de8";
+  let user = JSON.parse(localStorage.getItem('User'));
+
+  let owner_id = user.id;
   var request = new XMLHttpRequest();
   request.open('POST', 'http://localhost:3020/schedule', true);
   request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
@@ -115,6 +117,30 @@ function deleteTask(scheduleId) {
 }
 
 
+function acceptTask(scheduleId) {
+  var request = new XMLHttpRequest();
+  let user = JSON.parse(localStorage.getItem('User'));
+
+  request.open('PUT', 'http://localhost:3020/schedule/' + scheduleId, true);
+  request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  request.send(JSON.stringify({
+    status: "Aceito",
+    rival: user.id
+  }));
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var res = JSON.parse(request.response);
+      getTasks();
+
+      //console.log(res);
+    } else {
+      // We reached our target server, but it returned an error
+      console.log('error');
+    }
+  };
+
+}
 function getTasks() {
 
   let tasks = [];
@@ -128,6 +154,8 @@ function getTasks() {
     console.log("get")
     if (request.status === 200) {
       tasks =  JSON.parse(request.response);
+      //get user id localStorage
+      let user = JSON.parse(localStorage.getItem('User'));
       for (let i = 0; i < tasks.length; i++) {
         // let data = moment(tasks[i].date, 'YYYY-MM-DDTHH:mm:ss.SSSZ');
       let auxDate = tasks[i].dateConverted.split(" ");
@@ -137,9 +165,67 @@ function getTasks() {
         let team_rival = tasks[i].rival ? tasks[i].rival.team : "";
         let status = tasks[i].status;
         let scheduleId = tasks[i].id;
+        let owner_id = tasks[i].owner.id;
+        let rival_id = tasks[i].rival ? tasks[i].rival.id : "";
         // let horario = data.format('DD-MM-YYYY');      
-
-        tasksView.innerHTML +=
+        if (owner_id === user.id ) {
+          tasksView.innerHTML +=
+            `<div class="card mb-3">
+              <div class="card-body">
+              <div class="row">
+                <div class="col-sm-3 text-left">
+                  <p>${team_owner}</p>
+                  <p>X</p>
+                  <p>${team_rival}</p>
+                </div>
+                <div class="col-sm-3 text-left" style="margin-left:-1%; width:30px;">
+                  <p background-color="red">${date}</p>
+                  <p background-color="red">${horario}</p>
+                </div>
+                <div class="col-sm-3 text-left"  style="margin-left:-1%">
+                  <p>${status}</p>
+                </div>
+                <div class="col-sm-15 text-right"  style="z-index:0">
+                  <a href="#" onclick="deleteTask('${scheduleId}')" class="btn btn-danger ml-5" style="margin-top:-12%">X</a>
+                  <a href="#" onclick="loadTaskToUpdate('${tasks[i].dateConverted}','${scheduleId}')" class="btn btn-danger ml-2" style="
+                  margin-top:-12%; 
+                  margin-left:4%;
+                  background-color: #2ef3b1d8;
+                  color:black; 
+                  border-color:#2ef3b1d8"
+                  data-bs-toggle="modal" data-bs-target="#exampleModal"
+                  >
+                  <img src="/assets/pencil.svg" alt="" style="width: 100%">
+                  </a>
+                </div>
+              </div>  
+            </div>
+            </div>`;
+        } else if(user.id === rival_id ){
+          tasksView.innerHTML +=
+            `<div class="card mb-3">
+              <div class="card-body">
+              <div class="row">
+                <div class="col-sm-3 text-left">
+                  <p>${team_owner}</p>
+                  <p>X</p>
+                  <p>${team_rival}</p>
+                </div>
+                <div class="col-sm-3 text-left" style="margin-left:-1%; width:30px;">
+                  <p background-color="red">${date}</p>
+                  <p background-color="red">${horario}</p>
+                </div>
+                <div class="col-sm-3 text-left"  style="margin-left:-1%">
+                  <p>${status}</p>
+                </div>
+                <div class="col-sm-3 text-right flex center"  style="z-index:0">
+                  <p style="color:green; font-weight:bold">Agendado</p>
+                </div>
+              </div>  
+            </div>
+            </div>`;
+        } else if(user.id !== owner_id ) {
+          tasksView.innerHTML +=
           `<div class="card mb-3">
             <div class="card-body">
             <div class="row">
@@ -149,28 +235,20 @@ function getTasks() {
                 <p>${team_rival}</p>
               </div>
               <div class="col-sm-3 text-left" style="margin-left:-1%; width:30px;">
-                 <p background-color="red">${date}</p>
-                 <p background-color="red">${horario}</p>
-               </div>
+                <p background-color="red">${date}</p>
+                <p background-color="red">${horario}</p>
+              </div>
               <div class="col-sm-3 text-left"  style="margin-left:-1%">
                 <p>${status}</p>
               </div>
               <div class="col-sm-15 text-right"  style="z-index:0">
-                <a href="#" onclick="deleteTask('${scheduleId}')" class="btn btn-danger ml-5" style="margin-top:-12%">X</a>
-                <a href="#" onclick="loadTaskToUpdate('${tasks[i].dateConverted}','${scheduleId}')" class="btn btn-danger ml-2" style="
-                margin-top:-12%; 
-                margin-left:4%;
-                background-color: #2ef3b1d8;
-                color:black; 
-                border-color:#2ef3b1d8"
-                data-bs-toggle="modal" data-bs-target="#exampleModal"
-                >
-                <img src="/assets/pencil.svg" alt="" style="width: 100%">
-                </a>
+                <a href="#" onclick="acceptTask('${scheduleId}')" class="btn btn-danger ml-5" style="margin-top:-12%">Aceitar</a>                
               </div>
             </div>  
-           </div>
+          </div>
           </div>`;
+        }
+
     
       }
     }
